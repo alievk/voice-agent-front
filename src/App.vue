abc0ec3d3850cb4c402084994dd6bfa44fcfb10f
Ali Aliev <ali@avatarify.ai>
2024-10-16 14:54:15 +0000

    add seconds in timestamp

--- src/App.vue
+++ src/App.vue
@@ -31,6 +31,7 @@ export default {
       const timestamp = new Date().toLocaleString('en-GB', {
         hour: '2-digit',
         minute: '2-digit',
+        second: '2-digit',
         // day: '2-digit',
         // month: '2-digit',
         // year: 'numeric'
