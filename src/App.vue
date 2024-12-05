0c17c4b06cba06a1c77fcbc13e07d4b8ebff2b0b
Ali Aliev <alievk91@gmail.com>
2024-12-05 19:49:37 +0300

    get hostname and port from .env

--- src/App.vue
+++ src/App.vue
@@ -132,7 +132,10 @@ export default {
 
     async connect() {
       try {
-        await this.webSocketManager.connect(window.location.hostname)
+        await this.webSocketManager.connect(
+          process.env.VUE_APP_WS_HOST || "localhost",
+          process.env.VUE_APP_WS_PORT || 8564
+        )
         this.webSocketManager.onAudioMessage = this.handleAudioMessage;
         this.webSocketManager.onJsonMessage = this.handleJsonMessage;
         this.webSocketManager.onSystemMessage = this.addSystemMessage;
